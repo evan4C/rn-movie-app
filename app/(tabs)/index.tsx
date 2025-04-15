dee6a7807d3925f1a3c343bf22b41381069f0baf
evan <lifan1994@outlook.com>
2025-04-15 22:07:58 +0900

    develop search screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -38,7 +38,9 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
-        <Image source={icons.logo} className="w-12 h-10" />
+        <View className="w-full flex-row justify-center mt-20 items-center">
+          <Image source={icons.logo} className="w-12 h-10" />
+        </View>
 
         {moviesLoading ? (
           <ActivityIndicator
